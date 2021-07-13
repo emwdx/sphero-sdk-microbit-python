4dec1e306d36368e75fb55a90e6de57db1f56d90
Evan Weinberg <eweinberg@ssis.edu.vn>
2021-07-13 10:23:22 +0700

    adaptSDKFunctions
    Progress as of July 13

--- main.py
+++ main.py
@@ -1,25 +1,75 @@
+
 import board
 import busio
-import digitalio
 import time
-import sphero
+import math
 
-#uart = busio.UART(board.D1, board.D0, baudrate=115200)
-rvr = sphero.RVR(board.D1, board.D0)
+from api_sphero_message import Message
+from drive import raw_motors, drive_to_position_si
+uart = busio.UART(board.TX, board.RX, baudrate=115200)
+
+def send_command(did, cid, seq, target, timeout=None, inputs=[], outputs=[]):
+        """Creates a Message object using the provided parameters and sends it to the serial port.
+        Args:
+            did (uint8): Device ID
+            cid (uint8): Command ID
+            seq (uint8): Sequence Number
+            target (uint8): 1 - Nordic; 2 - ST
+            timeout (uint8): Time in seconds to wait for a response, if one is requested. Otherwise, ignored.
+            inputs (list(Parameter)): Inputs for command that is being sent
+            outputs (list(Parameter)): Expected outputs for command that is being sent
+        """
+
+        # TODO: implement timeout logic, which should remove any registered callbacks if timeout occurs.
+        message = Message()
+        message.did = did
+        message.cid = cid
+        message.seq = seq
+        message.target = target
+        message.is_activity = True
+        #message.requests_response = len(outputs) > 0 or self.request_error_responses_only
+
+        # Messages that already request a response due to expected outputs don't need this
+        # extra flag. They will automatically respond with errors if any are generated.
+        # This flag is meant only for commands with no expected output.
+        message.requests_error_response = True #self.request_error_responses_only if len(outputs) == 0 else False
+
+        for param in inputs:
+            message.pack(param.data_type, param.value)
+
+        return message
+
+time.sleep(1.0)
+SPEED = 0.4
+TILE_WIDTH = 0.8
+coordinates = [[0,0],[0,3],[2,3],[2,5],[0,5],[0,0]]
+positions = []
+for pair in coordinates:
+    positions.append([0.0,pair[0]*TILE_WIDTH,pair[1]*TILE_WIDTH])
+#positions = [[0.0,0.0,0.0],[90.0, 1.6,0.0],[0.0,1.6,0.8],[270.0,0,0.8],[0.0,0.0,0.0]]
+commands = [drive_to_position_si(0.0, 1.6, 0.0, 0.25, 0, None, None),drive_to_position_si(0.0, 1.6, 0.8, 0.25, 0, None, None),drive_to_position_si(0.0, 0, 0.8, 0.25, 0, None, None),drive_to_position_si(0.0, 0.0, 0.0, 0.25, 0, None, None)]
+
+for i in range(len(positions)):
+    current_position = positions[i]
+    next_position = positions[i+1]
+    command = drive_to_position_si(next_position[0],next_position[1],next_position[2],SPEED,0,None,None)
+    output = command
+    output_command = send_command(output['did'], output['cid'], output['seq'], output['target'], None, output['inputs'])
+
+    uart.write(output_command.serialise())
+    response = bytearray(10)
+    uart.readinto(response)
+    dx = next_position[1] - current_position[1]
+    dy = next_position[2] - current_position[2]
+    travel_time = math.sqrt(dx*dx + dy*dy)/SPEED + 1.5
+    print("Driving to {0},{1}, travel time {2}".format(next_position[1],next_position[2],travel_time))
+    time.sleep(travel_time)
 
 
-'''
-rvr.set_all_leds(255,0,0)
-rvr.drive(50,0)
-time.sleep(1.0)
-rvr.set_all_leds(0,255,0)
-rvr.drive(50,90)
-time.sleep(1.0)
-rvr.set_all_leds(255,255,0)
-rvr.drive(50,180)
-time.sleep(1.0)
-rvr.set_all_leds(0,255,255)
-rvr.drive(50,270)
-time.sleep(1.0)
-'''
-rvr.drive_to_position_si(0.0,0.5,2.0,50.0)
+#output = raw_motors(1,127,1,65,None,None)
+
+    #print(output_command.serialise())
+
+
+
+    #print(response)
