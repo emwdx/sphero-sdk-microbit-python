0e75b66a2b66152ac5eb6460885e54d977622e2e
Evan Weinberg <eweinberg@ssis.edu.vn>
2021-07-14 20:40:50 +0700

    vJul14a
    Complete demonstration of drive_to_position_si working on its own.

--- main.py
+++ main.py
@@ -3,73 +3,60 @@ import board
 import busio
 import time
 import math
+import struct
 
-from api_sphero_message import Message
-from drive import raw_motors, drive_to_position_si
-uart = busio.UART(board.TX, board.RX, baudrate=115200)
-
-def send_command(did, cid, seq, target, timeout=None, inputs=[], outputs=[]):
-        """Creates a Message object using the provided parameters and sends it to the serial port.
-        Args:
-            did (uint8): Device ID
-            cid (uint8): Command ID
-            seq (uint8): Sequence Number
-            target (uint8): 1 - Nordic; 2 - ST
-            timeout (uint8): Time in seconds to wait for a response, if one is requested. Otherwise, ignored.
-            inputs (list(Parameter)): Inputs for command that is being sent
-            outputs (list(Parameter)): Expected outputs for command that is being sent
-        """
-
-        # TODO: implement timeout logic, which should remove any registered callbacks if timeout occurs.
-        message = Message()
-        message.did = did
-        message.cid = cid
-        message.seq = seq
-        message.target = target
-        message.is_activity = True
-        #message.requests_response = len(outputs) > 0 or self.request_error_responses_only
-
-        # Messages that already request a response due to expected outputs don't need this
-        # extra flag. They will automatically respond with errors if any are generated.
-        # This flag is meant only for commands with no expected output.
-        message.requests_error_response = True #self.request_error_responses_only if len(outputs) == 0 else False
-
-        for param in inputs:
-            message.pack(param.data_type, param.value)
-
-        return message
 
 time.sleep(1.0)
-SPEED = 0.4
+
+uart = busio.UART(board.TX, board.RX, baudrate=115200)
+
+
+def drive_to_position_si(heading, x, y, speed):
+    SOP = 0x8d
+    FLAGS = 0x06
+
+    TARGET_ID = 0x0e
+    SOURCE_ID = 0x0b
+    DEVICE_ID = 0x16
+    COMMAND_ID = 0x38
+    SEQ = 0x01
+    EOP = 0xD8
+
+    yaw_angle = bytearray(struct.pack('>f', heading))
+    x = bytearray(struct.pack('>f', x))
+    y = bytearray(struct.pack('>f', y))
+    speed = bytearray(struct.pack('>f', speed))
+    flags = bytearray(struct.pack('B', 2))
+
+    output_packet = [SOP, FLAGS, DEVICE_ID,COMMAND_ID,SEQ]
+    output_packet.extend(yaw_angle)
+    output_packet.extend(x)
+    output_packet.extend(y)
+    output_packet.extend(speed)
+    output_packet.extend(flags)
+    output_packet.extend([~((sum(output_packet) - SOP) % 256) & 0x00FF,EOP])
+
+    print(bytearray(output_packet))
+    return bytearray(output_packet)
+
+SPEED = 0.6
 TILE_WIDTH = 0.8
-coordinates = [[0,0],[0,3],[2,3],[2,5],[0,5],[0,0]]
+#coordinates = [[0,0],[0,1.5],[1,1.5],[1,3],[2,3],[2,1],[3,0],[0,0]]
+coordinates = [[0,0],[0,2],[-1,2],[-1,3],[2,3],[2,2],[0,1],[0,0],[0,0.1]]
 positions = []
 for pair in coordinates:
     positions.append([0.0,pair[0]*TILE_WIDTH,pair[1]*TILE_WIDTH])
-#positions = [[0.0,0.0,0.0],[90.0, 1.6,0.0],[0.0,1.6,0.8],[270.0,0,0.8],[0.0,0.0,0.0]]
-commands = [drive_to_position_si(0.0, 1.6, 0.0, 0.25, 0, None, None),drive_to_position_si(0.0, 1.6, 0.8, 0.25, 0, None, None),drive_to_position_si(0.0, 0, 0.8, 0.25, 0, None, None),drive_to_position_si(0.0, 0.0, 0.0, 0.25, 0, None, None)]
 
-for i in range(len(positions)):
+for i in range(len(positions)-1):
     current_position = positions[i]
     next_position = positions[i+1]
-    command = drive_to_position_si(next_position[0],next_position[1],next_position[2],SPEED,0,None,None)
-    output = command
-    output_command = send_command(output['did'], output['cid'], output['seq'], output['target'], None, output['inputs'])
-
-    uart.write(output_command.serialise())
-    response = bytearray(10)
-    uart.readinto(response)
     dx = next_position[1] - current_position[1]
     dy = next_position[2] - current_position[2]
-    travel_time = math.sqrt(dx*dx + dy*dy)/SPEED + 1.5
-    print("Driving to {0},{1}, travel time {2}".format(next_position[1],next_position[2],travel_time))
+    raw_angle = 180.0/3.1415926*math.atan2(dx,dy)
+
+    command = drive_to_position_si(raw_angle,next_position[1],next_position[2],SPEED)
+    uart.write(command)
+
+    travel_time = math.sqrt(dx*dx + dy*dy)/SPEED*1.1 + 1.5
+    print("Driving to {0},{1}, heading {2}, travel time {3}".format(next_position[1],next_position[2], raw_angle,travel_time))
     time.sleep(travel_time)
-
-
-#output = raw_motors(1,127,1,65,None,None)
-
-    #print(output_command.serialise())
-
-
-
-    #print(response)
